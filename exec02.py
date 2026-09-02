536bbf175a87a21e14028985a3fffc22984b88b4
genildo alves <genildo.alves@escolar.ifrn.edu.br>
2026-09-01 21:00:48 -0300

    exec02

---
+++ exec02.py
@@ -0,0 +1,8 @@
+v1 = int(input("Informe idade em anos: "))
+v2 = int(input("Informe idade em meses: "))
+v3 = int(input("Informe idade em dias: "))
+
+t = v1 * 365 + v2 * 30 + v3
+
+
+print(f"o valor total em dias: {t}")
