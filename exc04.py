5c58321a13a0d34ab61811837a96a43d396b9f87
genildo alves <genildo.alves@escolar.ifrn.edu.br>
2026-09-01 21:25:33 -0300

    exec04

---
+++ exc04.py
@@ -0,0 +1,7 @@
+hr = int(input("informe valor da hora"))
+min = int(input("informe valor do minuto"))
+
+hr = hr * 60
+mint = hr + min
+print("o valor total em minutos é: ", hr)
+print("o valor total em minutos é: ", mint)
